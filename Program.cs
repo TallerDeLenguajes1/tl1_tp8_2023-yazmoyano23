1875be7ae63aa7d8db3000a61cc03e115dfc8678
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-06-10 03:09:37 -0300

    Segundo commit: Fin de la interfaz para mover tareas

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using tareas;
+
+List<Tarea> tareasPendientes = new List<Tarea>();
+List<Tarea> tareasRealizadas = new List<Tarea>();
+
+for (int N = 0; N < 5; N++)
+{
+    Tarea nuevaTarea = new Tarea();
+    nuevaTarea.Descripcion = "Tarea " + (N+1);
+    nuevaTarea.Duracion = N+5;
+    nuevaTarea.TareaID = N + 1;
+    tareasPendientes.Add(nuevaTarea);
+}
+
+string? resp = "";
+foreach (var tarea in tareasPendientes)
+{
+    Console.WriteLine("¿Se realizó la tarea " + tarea.TareaID + ": " + tarea.Descripcion + "?");
+    resp = Console.ReadLine();
+    if (resp == "S")
+    {
+        tareasRealizadas.Add(tarea);
+    }
+}
+
+foreach ( var tarea in tareasRealizadas) //Realizadas queda con dos 
+{
+    tareasPendientes.Remove(tarea); 
+}
+
+Console.WriteLine("\nTAREAS PENDIENTES");
+
+foreach (var Tarea in tareasPendientes)
+{
+    Console.WriteLine("\nID: " + Tarea.TareaID);
+    Console.WriteLine("Descripción: " + Tarea.Descripcion);
+    Console.WriteLine("Duración: " + Tarea.Duracion);
+}
+
+Console.WriteLine("\nTAREAS REALIZADAS");
+
+foreach (var Tarea in tareasRealizadas)
+{
+    Console.WriteLine("\nID: " + Tarea.TareaID);
+    Console.WriteLine("Descripción: " + Tarea.Descripcion);
+    Console.WriteLine("Duración: " + Tarea.Duracion);
+}
---
+++ Tarea.cs
@@ -0,0 +1,21 @@
+
+namespace tareas;
+
+public class Tarea
+{
+    private int tareaID;
+    private string? descripcion;
+    private int duracion;
+
+    public int Duracion { get => duracion; set => duracion = value; }
+    public string? Descripcion { get => descripcion; set => descripcion = value; }
+    public int TareaID { get => tareaID; set => tareaID = value; }
+}
+/*
+//No borrar dentro de un for
+//Las dos listas siguen apuntano a los mismos archivos, cuando paso las pe
+foreach ( item in realizadas) //Realizadas queda con dos 
+{
+    pendientes.Remove(item); 
+}
+*/
