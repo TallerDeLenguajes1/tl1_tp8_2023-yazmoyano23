45cdf3b6785080fdd70c7faf49496b4c06ed5bca
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-06-28 02:08:26 -0300

    Fin punto 1 Tareas

--- Program.cs
+++ Program.cs
@@ -1,49 +1,104 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System.IO;
 using tareas;
 
-List<Tarea> tareasPendientes = new List<Tarea>();
-List<Tarea> tareasRealizadas = new List<Tarea>();
-
-for (int N = 0; N < 5; N++)
+internal class Program
 {
-    Tarea nuevaTarea = new Tarea();
-    nuevaTarea.Descripcion = "Tarea " + (N+1);
-    nuevaTarea.Duracion = N+5;
-    nuevaTarea.TareaID = N + 1;
-    tareasPendientes.Add(nuevaTarea);
-}
-
-string? resp = "";
-foreach (var tarea in tareasPendientes)
-{
-    Console.WriteLine("¿Se realizó la tarea " + tarea.TareaID + ": " + tarea.Descripcion + "?");
-    resp = Console.ReadLine();
-    if (resp == "S")
+    private static void Main(string[] args) 
     {
-        tareasRealizadas.Add(tarea);
+        List<Tarea> tareasPendientes = new List<Tarea>();
+        List<Tarea> tareasRealizadas = new List<Tarea>();
+
+        string?[] descripcion = {"Preparar informes", "Preparar Documentacion", "Programar tareas"};
+
+        for (int N = 0; N < 3; N++)
+        {
+            Tarea nuevaTarea = new Tarea();
+            nuevaTarea.Descripcion = descripcion[N];
+            nuevaTarea.Duracion = N+3;
+            nuevaTarea.TareaID = N + 1;
+            tareasPendientes.Add(nuevaTarea);
+        }
+
+        string? resp = "", tareaBuscar="";
+
+        foreach (var tarea in tareasPendientes)
+        {
+            Console.WriteLine("¿Se realizó la tarea " + tarea.TareaID + ": " + tarea.Descripcion + "?");
+            resp = Console.ReadLine();
+            if (resp == "S")
+            {
+                tareasRealizadas.Add(tarea);
+            }
+        }
+
+        foreach ( var tarea in tareasRealizadas) //Realizadas queda con dos 
+        {
+            tareasPendientes.Remove(tarea); 
+        }
+
+        Console.WriteLine("\nTAREAS PENDIENTES");
+
+        foreach (var Tarea in tareasPendientes)
+        {
+            Console.WriteLine("\nID: " + Tarea.TareaID);
+            Console.WriteLine("Descripción: " + Tarea.Descripcion);
+            Console.WriteLine("Duración: " + Tarea.Duracion);
+        }
+
+        Console.WriteLine("\nTAREAS REALIZADAS");
+
+        foreach (var Tarea in tareasRealizadas)
+        {
+            Console.WriteLine("\nID: " + Tarea.TareaID);
+            Console.WriteLine("Descripción: " + Tarea.Descripcion);
+            Console.WriteLine("Duración: " + Tarea.Duracion);
+        }
+
+
+        GuardarArchivo(tareasPendientes);
+
+        Console.WriteLine("\nBuscar tarea... (SI:S NO:N) ");
+        resp = Console.ReadLine();
+        if (resp == "S")
+        {
+            Console.WriteLine("Ingrese la descripcion de la tarea a buscar:");
+            tareaBuscar = Console.ReadLine();
+            foreach (var Tarea in tareasPendientes)
+            {
+                if (tareaBuscar == Tarea.Descripcion)
+                {
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine("Informacion de tarea:");
+                    Console.WriteLine("ID: " + Tarea.TareaID);
+                    Console.WriteLine("Descripción: " + Tarea.Descripcion);
+                    Console.WriteLine("Duración: " + Tarea.Duracion);
+                } 
+
+            }
+
+        } else {
+                Console.WriteLine("FIN");
+        }
+
+        
+
+    }
+
+    private static void GuardarArchivo(List<Tarea> tareas)
+    {
+        int suma = 0;
+        string? ruta = @"C:\Users\yazmi\tpTaller\tl1_tp8_2023-yazmoyano23\";
+
+        foreach (var tarea in tareas)
+        {
+            suma += tarea.Duracion;
+        }
+
+        StreamWriter escribe = new StreamWriter(ruta + "cantidadhorastrabajadas", true);
+        escribe.WriteLine("Horas trabajadas: " + suma);
+        escribe.Close();
     }
 }
 
-foreach ( var tarea in tareasRealizadas) //Realizadas queda con dos 
-{
-    tareasPendientes.Remove(tarea); 
-}
 
-Console.WriteLine("\nTAREAS PENDIENTES");
-
-foreach (var Tarea in tareasPendientes)
-{
-    Console.WriteLine("\nID: " + Tarea.TareaID);
-    Console.WriteLine("Descripción: " + Tarea.Descripcion);
-    Console.WriteLine("Duración: " + Tarea.Duracion);
-}
-
-Console.WriteLine("\nTAREAS REALIZADAS");
-
-foreach (var Tarea in tareasRealizadas)
-{
-    Console.WriteLine("\nID: " + Tarea.TareaID);
-    Console.WriteLine("Descripción: " + Tarea.Descripcion);
-    Console.WriteLine("Duración: " + Tarea.Duracion);
-}
--- Tarea.cs
+++ Tarea.cs
@@ -10,7 +10,9 @@ public class Tarea
     public int Duracion { get => duracion; set => duracion = value; }
     public string? Descripcion { get => descripcion; set => descripcion = value; }
     public int TareaID { get => tareaID; set => tareaID = value; }
+
 }
+
 /*
 //No borrar dentro de un for
 //Las dos listas siguen apuntano a los mismos archivos, cuando paso las pe
